aab879ca3cbdcbc8edff81a3e1507d7e2c3d3a0e
dan <ec21515@qmul.ac.uk>
2023-03-19 23:10:35 +0000

    Updated framework

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,9 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="../../scss/custom.css" />
+      </Head>
       <body>
-        <Main />
+        <div className="container">
+          <Main />
+        </div>
         <NextScript />
       </body>
     </Html>
