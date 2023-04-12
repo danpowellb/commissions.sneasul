a30f8a915760c0708a32f88d2e681bde0a53c6c2
dan <ec21515@qmul.ac.uk>
2023-04-13 00:08:05 +0100

    Regenerate framework

--- commissions.sneasul/src/pages/_document.tsx
+++ commissions.sneasul/src/pages/_document.tsx
@@ -3,13 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="stylesheet" href="../../scss/custom.css" />
-      </Head>
+      <Head />
       <body>
-        <div className="container">
-          <Main />
-        </div>
+        <Main />
         <NextScript />
       </body>
     </Html>
